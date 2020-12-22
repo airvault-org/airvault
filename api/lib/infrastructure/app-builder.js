70771c00926ee36c5fc5555457533c4de9dd7fb9
jbuget <jbuget@gmail.com>
2020-12-22 19:32:27 +0100

    Add fastify-helmet for security

--- api/lib/infrastructure/app-builder.js
+++ api/lib/infrastructure/app-builder.js
@@ -1,18 +1,26 @@
 const fastify = require('fastify');
+const helmet = require('fastify-helmet')
+
 const IocContainer = require('./ioc-container');
 const PassRepositorySql = require('./repositories/PassRepositorySql');
 
-function build(opts={}) {
+function build(opts = {}) {
 
   // See https://nodejs.org/api/net.html#net_server_listen_options_callback
   const app = fastify(opts);
+
+  app.register(helmet);
 
   const container = new IocContainer();
   container.register('PassRepository', new PassRepositorySql());
   app.decorate('container', container);
 
-  require('./routes/root').forEach(route => { app.route(route); });
-  require('./routes/passes').forEach(route => { app.route(route); });
+  require('./routes/root').forEach(route => {
+    app.route(route);
+  });
+  require('./routes/passes').forEach(route => {
+    app.route(route);
+  });
 
   return app;
 }
