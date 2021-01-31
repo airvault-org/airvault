69646aa02bf65d3b4a3121e64fc7ff73e98ec951
jbuget <jbuget@gmail.com>
2021-01-31 14:03:29 +0100

    [webapp] Display item summary list

--- webapp/src/views/Dashboard.vue
+++ webapp/src/views/Dashboard.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="home">
-    <h1>This is user's dashboard page</h1>
-    {{ items }}
+    <div id="item-summary-list">
+      <ul>
+        <li v-for="item in items" :key="item.id">
+          <div class="item-summary">
+            <span class="item-summary__title">{{item.title}}</span>
+            <span class="item-summary__username">{{item.username}}</span>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -22,3 +30,32 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+ul, li, ol {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+.item-summary-list {
+
+}
+
+.item-summary {
+  padding: 10px;
+}
+
+.item-summary:hover {
+  background-color: #F6F7FB;
+}
+
+.item-summary__title {
+  display: block;
+  font-weight: 600;
+}
+.item-summary__username {
+  display: block;
+}
+
+</style>
