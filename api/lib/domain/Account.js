16138cff6d395496215720a96068555a51ac8081
jbuget <jbuget@gmail.com>
2021-02-04 16:38:18 +0100

    [api] Add field `uuid` in each domain object

--- api/lib/domain/Account.js
+++ api/lib/domain/Account.js
@@ -1,6 +1,7 @@
 class Account {
 
   id;
+  uuid;
   username;
   email;
   createdAt;
@@ -8,6 +9,7 @@ class Account {
 
   constructor(attributes = {}) {
     this.id = attributes.id;
+    this.uuid = attributes.uuid;
     this.username = attributes.username;
     this.email = attributes.email;
     this.createdAt = attributes.createdAt;
--- api/lib/domain/VaultSummary.js
+++ api/lib/domain/VaultSummary.js
@@ -1,11 +1,13 @@
 class VaultSummary {
 
   id;
+  uuid;
   name;
   itemsCount;
 
   constructor(attributes = {}) {
     this.id = attributes.id;
+    this.uuid = attributes.id;
     this.name = attributes.name;
     this.itemsCount = attributes.itemsCount;
   }
