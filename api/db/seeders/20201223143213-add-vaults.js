8151feab9b4b772db646a4d830e8dfe273bc3875
jbuget <jbuget@gmail.com>
2021-02-04 15:12:39 +0100

    [db] Remove useless comments

--- api/db/seeders/20201223143213-add-vaults.js
+++ api/db/seeders/20201223143213-add-vaults.js
@@ -2,15 +2,6 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
     return queryInterface.bulkInsert('vaults', [{
       name: 'Default',
       createdAt: new Date(),
@@ -25,12 +16,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
     await queryInterface.dropTable('vaults');
   }
 };
