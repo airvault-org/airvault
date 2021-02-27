730566abbcf33665843e0a13b6f1c74cb8d8b19a
jbuget <jbuget@gmail.com>
2021-02-27 18:29:40 +0100

    [webapp] Create component <item-summary-list/>

---
+++ webapp/src/components/ItemSummaryList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="item-summary-list">
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        <div class="item-summary">
+          <span class="item-summary__title">{{ item.title }}</span>
+          <span class="item-summary__username">{{ item.username }}</span>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['items'],
+}
+</script>
+
+<style>
+
+ul, li, ol {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+.item-summary-list {
+
+}
+
+.item-summary {
+  padding: 10px;
+}
+
+.item-summary:hover {
+  background-color: #F6F7FB;
+}
+
+.item-summary__title {
+  display: block;
+  font-weight: 600;
+}
+
+.item-summary__username {
+  display: block;
+}
+</style>
