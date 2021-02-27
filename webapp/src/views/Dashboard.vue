58db6eb7ca4a702f4d73387c13ce53d15f041ccb
jbuget <jbuget@gmail.com>
2021-02-27 11:31:37 +0100

    [webapp] Load authenticated user vaults in dashboard

--- webapp/src/views/Dashboard.vue
+++ webapp/src/views/Dashboard.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="home">
-    <div id="item-summary-list">
-      <ul>
-        <li v-for="item in items" :key="item.id">
-          <div class="item-summary">
-            <span class="item-summary__title">{{item.title}}</span>
-            <span class="item-summary__username">{{item.username}}</span>
-          </div>
-        </li>
-      </ul>
+
+    <div class="panel">
+      <div id="vault-summary-list">
+        <ul>
+          <li v-for="vault in vaults" :key="vault.id">
+            <div class="vault-summary">
+              <span class="vault-summary__title">{{vault.name}}</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="panel">
+      <div id="item-summary-list">
+        <ul>
+          <li v-for="item in items" :key="item.id">
+            <div class="item-summary">
+              <span class="item-summary__title">{{item.title}}</span>
+              <span class="item-summary__username">{{item.username}}</span>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -18,14 +33,18 @@ export default {
   computed: {
     items () {
       return this.$store.getters.items
-    }
+    },
+    vaults () {
+      return this.$store.getters.vaults
+    },
   },
   created () {
     this.fetchData()
   },
   methods: {
-    fetchData () {
-      return this.$store.dispatch('fetchItems')
+    async fetchData () {
+      await this.$store.dispatch('fetchVaults')
+      await this.$store.dispatch('fetchItems')
     }
   }
 }
@@ -36,6 +55,15 @@ ul, li, ol {
   margin: 0;
   padding: 0;
   list-style-type: none;
+}
+
+.home {
+  display: flex;
+  flex-direction: row;
+}
+
+.panel {
+  min-width: 400px;
 }
 
 .item-summary-list {
