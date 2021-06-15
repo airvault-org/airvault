859080381244c777cc1cbdc3288179d19c0c5c7f
jbuget <jbuget@gmail.com>
2021-06-15 15:40:49 +0200

    [webapp] Redseign Login page

--- webapp/src/views/Login.vue
+++ webapp/src/views/Login.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="login rounded-panel">
-    <h1>Sign in with your account</h1>
-    <form class="form" @submit="login">
-      <div class="form__field">
-        <label class="form__label" for="email">Email</label>
-        <input class="form__input" v-model="username" id="email" type="email">
-      </div>
-      <div class="form__field">
-        <label class="form__label" for="password">Password</label>
-        <input class="form__input" v-model="password" id="password" type="password">
-      </div>
-      <input class="form__submit" type="submit" value="Submit">
+  <div>
+    <form novalidate class="md-layout md-alignment-center-center" @submit.prevent="login">
+      <md-card class="md-layout-item md-size-30 md-small-size-100">
+        <md-card-header>
+          <div class="md-title">Sign in with your account</div>
+        </md-card-header>
+
+        <md-card-content>
+          <md-field>
+            <label for="email">Email</label>
+            <md-input type="email" name="email" id="email" autocomplete="email" v-model="form.username" :disabled="sending"/>
+          </md-field>
+          <md-field>
+            <label for="password">Password</label>
+            <md-input type="password" name="password" id="password" autocomplete="off" v-model="form.password" :disabled="sending"/>
+          </md-field>
+        </md-card-content>
+
+        <md-progress-bar md-mode="indeterminate" v-if="sending"/>
+
+        <md-card-actions>
+          <md-button type="submit" class="md-primary" :disabled="sending">Log in</md-button>
+        </md-card-actions>
+      </md-card>
     </form>
   </div>
 </template>
@@ -21,17 +33,19 @@ import api from '../services/api'
 export default {
   data() {
     return {
-      username: null,
-      password: null,
+      form: {
+        username: null,
+        password: null,
+      },
+      sending: false
     }
   },
   methods: {
-    async login(e) {
-      e.preventDefault()
-      if (this.username && this.password) {
+    async login() {
+      if (this.form.username && this.form.password) {
         await api.authenticate({
-          username: this.username,
-          password: this.password,
+          username: this.form.username,
+          password: this.form.password,
         })
         this.$router.push('/items')
       }
