75ebdfd7f7d05a9a227d0fc1f2217a710ecdaac0
jbuget <jbuget@gmail.com>
2021-06-15 15:39:25 +0200

    [webapp] Add Vue Material

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -5,6 +5,11 @@ import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
 
 library.add(fas)
 
