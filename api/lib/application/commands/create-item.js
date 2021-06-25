13e0e0ee2ee1a49eee70617855ad253b991c6389
jbuget <jbuget@gmail.com>
2021-06-25 19:23:08 +0200

    [api] Convert Item domain model object

--- api/lib/application/commands/create-item.js
+++ api/lib/application/commands/create-item.js
@@ -1,5 +1,5 @@
 const uuidv4 = require('uuid').v4;
-const Item = require('../../domain/Item');
+const { Item, ItemContent } = require('../../domain/Item');
 const ItemType = require('../../domain/ItemType');
 
 module.exports = ({ vaultUuid, title, username, password, website } = {}, iocContainer) => {
@@ -8,7 +8,9 @@ module.exports = ({ vaultUuid, title, username, password, website } = {}, iocCon
 
   const now = new Date();
 
-  const item = new Item({ uuid: uuidv4(), type: ItemType.LOGIN, title, username, password, website, createdAt: now, updatedAt: now, vaultUuid });
+  const itemContent = new ItemContent(title, username, password, website, );
+
+  const item = new Item({ uuid: uuidv4(), type: ItemType.LOGIN, content: itemContent, createdAt: now, updatedAt: now, vaultUuid });
 
   return itemRepository.save(item);
 }
