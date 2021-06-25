3d90b478f3061f1f93123c347a2a01a7ed1f9b6f
jbuget <jbuget@gmail.com>
2021-06-26 00:50:02 +0200

    [api] Convert Sequelize models
    Added @types/validator as preconised in the documentation.

---
+++ api/db/models/account.ts
@@ -0,0 +1,54 @@
+import {DataTypes, ModelDefined, Optional, Sequelize} from 'sequelize';
+
+interface AccountAttributes {
+  id: number;
+  uuid: string;
+  name: string;
+  username: string;
+  encryptedPassword: string;
+  email: string;
+}
+
+interface AccountCreationAttributes extends Optional<AccountAttributes, 'id'> {
+}
+
+function build(db: any): ModelDefined<AccountAttributes, AccountCreationAttributes> {
+
+  const Account = db.sequelize.define('Account', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    uuid: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: false
+    },
+    name: {
+      type: new DataTypes.STRING(128),
+      allowNull: false,
+    },
+    username: {
+      type: new DataTypes.STRING(128),
+      allowNull: false,
+    },
+    encryptedPassword: {
+      type: new DataTypes.STRING(128),
+      allowNull: false,
+    },
+    email: {
+      type: new DataTypes.STRING(128),
+      allowNull: false,
+      unique: true,
+    },
+  }, {
+    tableName: 'accounts',
+  });
+
+  db[Account.name] = Account;
+
+  return Account;
+}
+
+export {build}
