baf725fc0bd5ba93701bb53130e41f641b2f7b50
jbuget <jbuget@gmail.com>
2021-10-29 00:54:29 +0200

    [api] Use Map instead of Record
    cf. https://stackoverflow.com/a/18541990

--- api/lib/infrastructure/ioc.ts
+++ api/lib/infrastructure/ioc.ts
@@ -1,18 +1,18 @@
 class IocContainer {
 
-  private readonly serviceMap: Record<string, any>;
+  private readonly serviceMap: Map<string, any>;
 
   constructor() {
-    this.serviceMap = {};
+    this.serviceMap = new Map();
   }
 
   register(serviceName: string, service: any) {
-    this.serviceMap[serviceName] = service;
+    this.serviceMap.set(serviceName, service);
     return service;
   }
 
   get(serviceName: string) {
-    const service = this.serviceMap[serviceName];
+    const service = this.serviceMap.get(serviceName);
     if (!service) {
       throw new Error(`Service ${serviceName} not found in IOC container`);
     }
